f3f576465a498c1fca1321b225839fdc0ffa19e8
swapnilchavada <30380804+swapnilchavada@users.noreply.github.com>
2019-10-01 12:31:00 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,27 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@swapnilchavada 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+
+0
+01swapnilchavada/messenger-webhook-post
+ Code Issues 0 Pull requests 0 Projects 0 Wiki Security Insights Settings
+messenger-webhook-post/app/routes.py
+@swapnilchavada swapnilchavada Update routes.py
+f45163a on Apr 21
+We found potential security vulnerabilities in your dependencies.
+Only the owner of this repository can see this message.
+
+186 lines (162 sloc)  5.91 KB
+  
 from app import app
 from flask import make_response, request
 
@@ -6,9 +30,13 @@ import requests
 
 WEBHOOK_VERIFY_TOKEN = 'test_faq_token'
 PAGE_ACCESS_TOKEN = 'EAAeeVSYcYQYBAHng9dsVAiRdvy1kKttTvWFkcmoU2fwUr44vnJXVQJfXzuZCL8FAUM9IqZCqlZBgaWMbRw6JQ09xlF9s130Syzt1trfbZCC1jxbM4IHtPqxVXRg6DB11rsDheteFCtLuXHqJWTXqHkz4gMV7QZBRATgucFmF2ywZDZD'
+#this is for page Page_thread_queue_test
+PAGE_ACCESS_TOKEN2 = 'EAAeeVSYcYQYBAOVAMY6T8t6htQMnJ3gGZBfq9H7VsvRaazNsqJ6FIfsIYK2GBAWRNrFzyB95BADbeZBZClDP6Vdf7Jp7gtpYIIur4oPZCBl4VXpAf3P4kjM8ldR3heOXbUZCFBzvk6rfB0iTPOlqhSKUySb8afVp8rLNnBMR1O0E71D1vfOlvAu3g0lvGRAkZD'
 
 SEND_API_URL = 'https://graph.facebook.com/v2.12/me/messages?access_token=%s'\
   % PAGE_ACCESS_TOKEN
+SEND_API_URL2 = 'https://graph.facebook.com/v2.12/me/messages?access_token=%s'\
+  % PAGE_ACCESS_TOKEN2
 
 PASS_THREAD_CONTROL_URL = 'https://graph.facebook.com/v2.12/me/pass_thread_control?access_token=%s'\
   % PAGE_ACCESS_TOKEN
@@ -22,23 +50,45 @@ PAGE_INBOX = 263902037430900
 
 ME = '620697518375534'
 
-@app.route('/')
-@app.route('/index')
-def index():
-  print('send_message in indexx')
-  return 'Hello, World!'
-
 def send_message(body):
   print('send_message')
   print(body)
   try:
-    #send_message_to_recipient(json.dumps(body), sender)
+    for entry in body['entry']:
+        if 'messaging' in entry:
+          channel = 'messaging'
+        else:
+          channel = 'standby'
+        for message in entry[channel]:
+          sender = message['sender']['id']
+          recipient_id =  message['recipient']['id']
+          
+          if 'message' in message: 
+            webhook_type='message'
+          elif 'postback' in message:
+            webhook_type='postback' 
+          else:
+            return
+          if 'text' in message[webhook_type]:
+            msg_text = message[webhook_type]['text']
+            if 'echoing_back' in msg_text:
+              return
+          body['echoing_back'] = 'true'
+          print('sender1111')
+          if 'is_echo' in message[webhook_type]:
+            send_message_to_recipient(json.dumps(body), recipient_id, sender)
+          else:
+            send_message_to_recipient(json.dumps(body), sender, recipient_id)
+          print('sender')
+          print(sender)
+          return
     print('sender')
   except Exception as e:
      print("swapnilc-Exception sending")
      print(e)
-     
-def send_message_to_recipient(message_text, recipient_id):
+      
+      
+def send_message_to_recipient(message_text, recipient_id, page_id):
   message = {
     'recipient': {
       'id': recipient_id,
@@ -47,11 +97,95 @@ def send_message_to_recipient(message_text, recipient_id):
       'text': message_text,
     },
   }
-  r = requests.post(SEND_API_URL, data=json.dumps(message), headers=HEADERS)
+  r = requests.post(SEND_API_URL if page_id == '620697518375534' else SEND_API_URL2, data=json.dumps(message), headers=HEADERS)
+  if r.status_code != 200:
+    print('== ERROR====')
+    print(SEND_API_URL)
+    print(r.json())
+    print('==============')
+
+def pass_thread_control(app_id, recipient_id):
+  payload = {
+    'recipient': {
+      'id': recipient_id,
+    },
+    'target_app_id': app_id,
+  }
+  r = requests.post(PASS_THREAD_CONTROL_URL, data=json.dumps(payload), headers=HEADERS)
   if r.status_code != 200:
     print('====ERROR====')
     print(r.json())
+    print(r.request.data)
     print('==============')
+
+def take_thread_control(recipient_id):
+  payload = {
+    'recipient': {
+      'id': recipient_id,
+    },
+  }
+  r = requests.post(TAKE_THREAD_CONTROL_URL, data=json.dumps(payload), headers=HEADERS)
+  if r.status_code != 200:
+    print('====ERROR====')
+    print(r.json())
+    print(r.request.data)
+    print('==============')
+
+def pass_thread_control_to_page_inbox(recipient_id):
+  pass_thread_control(PAGE_INBOX, recipient_id)
+
+def handle_messaging_event(message):
+  sender = message['sender']['id']
+  if sender == ME:
+    return
+
+  if 'request_thread_control' in message:
+    send_message_to_recipient('Transferring you to live agent.', sender)
+    pass_thread_control(message['request_thread_control']['requested_owner_app_id'], sender)
+    return
+
+  if 'message' not in message:
+    return
+  if 'text' not in message['message']:
+    return
+  message_text = message['message']['text']
+  if 'return policy' in message_text:
+    send_message_to_recipient('We offer full refunds up to 30 days, with a valid receipt.', sender)
+  elif 'address' in message_text:
+    send_message_to_recipient('You can find us at 123 Hacker Way, Menlo Park, CA 94025.', sender)
+  elif 'hours' in message_text:
+    send_message_to_recipient('We\'re open 9:00 AM - 5:30 PM from Monday to Saturday.', sender)
+  else:
+    send_message_to_recipient('I\'m sorry, I am not able to help with that. I\'ll transfer you over to a live agent.', sender)
+    pass_thread_control_to_page_inbox(sender)
+
+def handle_standby_event(standby_event):
+  if 'sender' not in standby_event:
+    return
+  if 'id' not in standby_event['sender']:
+    return
+  sender = standby_event['sender']['id']
+  if 'message' not in standby_event:
+    return
+  if 'text' not in standby_event['message']:
+    return
+  message_text = standby_event['message']['text']
+  if 'deliver' in message_text:
+    take_thread_control(sender)
+    send_message_to_recipient('I noticed you asking about delivery options. Let me redirect you to our automated delivery processing service.', sender)
+
+def handle_entry(entry):
+  if 'messaging' in entry:
+    for message in entry['messaging']:
+      handle_messaging_event(message)
+  elif 'standby' in entry:
+    for standby_event in entry['standby']:
+      handle_standby_event(standby_event)
+
+@app.route('/')
+@app.route('/index')
+def index():
+  return 'Hello, World!'
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
@@ -73,3 +207,15 @@ def webhook():
     send_message(body)
     return ("", 205)
 
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
