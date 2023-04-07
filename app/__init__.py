696d39db14c9e3e97ffa20d368cf78c67d00d8a7
Swapnil Chavada <swapnilc@fb.com>
2023-04-07 13:32:59 -0700

    Asas

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 import os
 # from config import Config, ProductionConfig, DevelopmentConfig
-from config_values import DevelopmentConfig, ProductionConfig
+from .config_values import DevelopmentConfig, ProductionConfig
 
 app = Flask(__name__)
 c = DevelopmentConfig()
