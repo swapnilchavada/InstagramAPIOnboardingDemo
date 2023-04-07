4488b8286f7699429e920c351c4842d311a9d598
Swapnil Chavada <swapnilc@fb.com>
2023-04-07 09:45:26 -0700

    Sdsd

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ import os
 from config import DevelopmentConfig
 
 app = Flask(__name__)
-app.config['PAGE_ACCESS_TOKEN'] = os.environ.get('PAGE_ACCESS_TOKEN')
-c = ProductionConfig()
-app.config.from_object(c)
+# app.config['PAGE_ACCESS_TOKEN'] = os.environ.get('PAGE_ACCESS_TOKEN')
+# c = ProductionConfig()
+# app.config.from_object(c)
 
 from app import routes
